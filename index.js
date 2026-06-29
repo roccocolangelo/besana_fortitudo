dcd6b7045bee65b6f6bc5518c7f63ce065229b4d
rocco.colangelo@ing.com <rocco.colangelo@ing.com>
2026-06-29 16:21:17 +0200

    sanzioni section

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function openModal() {
 function closeModal() {
     document.getElementById("modal").style.display = "none";
 }
+
+let isLogged = false;
+
+const form = document.getElementById("form-sanzione");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!isLogged) {
+    document.getElementById("loginModal").style.display = "block";
+    return;
+  }
+
+  salvaSanzione();
+});
+
+function closeLogin() {
+  document.getElementById("loginModal").style.display = "none";
+}
